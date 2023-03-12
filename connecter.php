1af4eda4e5884c4dd4016410c292bf6ec0ae1556
Caro639 <120673222+Caro639@users.noreply.github.com>
2023-03-12 16:54:58 +0100

    page de connexion user

--- connecter.php
+++ connecter.php
@@ -1,7 +1,5 @@
+<?php session_start(); ?>
 
-<?php
-    include_once('connexion.php');
-?>
 
 <!DOCTYPE html>
 <html>
@@ -12,29 +10,36 @@
     </head>
 
     <body>
-    <?php include('header.php'); ?>
+        <div class="gbaf"><img src="images/logo_gbaf.png" alt="Logo de GBAF" />
+            <button><a href="inscription.php">Créer un compte</a></button>
+            <button><a href="form_mp.php">Mot de passe oublié</a></button>
+           
+        </div>
         <div id="container">
 
         <form action="verification.php" method="POST">
             <h1>Connexion</h1>
+            <?php if(isset($_SESSION['id_user']) && $_SESSION['id_user'] !== ''){ ?>
+                <p>Vous étes déja connecté</p>
+                <p> <a href='index.php?deconnexion=true'><span>Se déconnecter</span></a></p>
+            <?php } else { ?>
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
-            <label><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+                <label><b>Mot de passe</b></label>
+                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-            <label><b>Mot de passe</b></label>
-            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-            <input type="submit" id='submit' value='CONNECTER'>
-            <?php
-            if(isset($_GET['erreur'])){
-                $err = $_GET['erreur'];
-                if($err==1 || $err==2)
-                echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-            }
-            ?>
-            </form>
-            <button><a href="inscription.php">Créer un compte</a></button>
-            <button>Mot de passe oublié</button>
- 
+                <input type="submit" id='submit' value='CONNECTER'>
+                <?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                    echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                 
+                ?>
+            <?php  } ?>
+            
+        </form>
     </body>
 </html>
